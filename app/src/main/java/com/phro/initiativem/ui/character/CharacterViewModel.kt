1fb2d0e663097ca98256b6713016a3ee667cf79e
Pedro Henrique Rodrigues de Oliveira <pedronoveseis@gmail.com>
2023-12-28 22:26:09 -0300

    [INIM-003] Compose migration

--- app/src/main/java/com/phro/initiativem/ui/character/CharacterViewModel.kt
+++ app/src/main/java/com/phro/initiativem/ui/character/CharacterViewModel.kt
@@ -5,20 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.phro.initiativem.model.Results
 import com.phro.initiativem.repository.CharacterRepository
-import org.koin.core.KoinComponent
-import org.koin.core.inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 class CharacterViewModel : ViewModel(), KoinComponent {
 
     private val characterRepository: CharacterRepository by inject()
 
-    private val _characterLiveData = MutableLiveData<List<Results>>()
-    val characterLiveData: LiveData<List<Results>>
-        get() = _characterLiveData
+    private val _characterStateFlow = MutableStateFlow<List<Results>?>(null)
+    val characterStateFlow: StateFlow<List<Results>?> = _characterStateFlow
 
     fun getCharacterByName(name: String) {
         characterRepository.getCharacter(name) {
-            _characterLiveData.postValue(it)
+            _characterStateFlow.value = it
         }
     }
 }
--- app/src/main/java/com/phro/initiativem/ui/character/DetailCharacterActivity.kt
+++ app/src/main/java/com/phro/initiativem/ui/character/DetailCharacterActivity.kt
@@ -1,64 +1,97 @@
 package com.phro.initiativem.ui.character
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import androidx.appcompat.app.AlertDialog
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
-import com.bumptech.glide.Glide
-import com.phro.initiativem.databinding.ActivityDetailCharacterBinding
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.myapplication.ui.theme.InitiativeMTheme
 
 class DetailCharacterActivity : AppCompatActivity() {
-
-    private lateinit var characterBinding: ActivityDetailCharacterBinding
-    private lateinit var view: View
-    // Realizando a injeção da minha VM na minha Activity
-    /**
-     * Doc do Koin:
-     * The viewModel keyword helps declaring a factory instance of ViewModel.
-     * This instance will be handled by internal ViewModelFactory
-     * and reattach ViewModel instance if needed.
-     */
-//    private val detailViewModel: CharacterViewModel by viewModel()
-
-    // Provendo a VM para o meu escopo.
-    // Funciona sem, mas usar o Provider é uma recomendação da Google.
-    private val detailViewModel by lazy {
-        ViewModelProvider(this).get(CharacterViewModel::class.java)
+    private val viewModel by lazy {
+        ViewModelProvider(this)[CharacterViewModel::class.java]
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        characterBinding = ActivityDetailCharacterBinding.inflate(layoutInflater)
-        view = characterBinding.root
-        setContentView(view)
-
-        detailViewModel.getCharacterByName("Thanos")
-
-        observeCharacterLiveData()
-    }
-
-    private fun observeCharacterLiveData() {
-        detailViewModel.characterLiveData.observe(this) {
-
-            if (it.isNotEmpty()) {
-                characterBinding.txtName.text = it[0].name
-                characterBinding.txtDescription.text = it[0].description
-                Glide
-                    .with(this)
-                    .load(it[0].thumbnail.path + ".jpg")
-                    .into(characterBinding.thumbnail)
-            } else {
-                val buildDialog = AlertDialog.Builder(this)
-                    .setMessage("Tivemos um erro!")
-                    .setNegativeButton("Close") { dialog, _ ->
-                        dialog.dismiss()
-                    }
-                buildDialog.create().show()
+        setContent {
+            InitiativeMTheme {
+                ShowContent(Modifier.fillMaxSize())
             }
         }
+        viewModel.getCharacterByName("Thanos")
     }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    @Composable
+    private fun ShowContent(modifier: Modifier = Modifier) {
+        val characterState = viewModel.characterStateFlow.collectAsStateWithLifecycle()
+
+        Surface(
+            modifier = modifier,
+            color = MaterialTheme.colorScheme.background
+        ) {
+            CharacterTitle(characterTitle = characterState.value?.get(0)?.name ?: "Empty")
+        }
+
+    }
+
+    // Preview Parameter - https://foso.github.io/Jetpack-Compose-Playground/general/preview/previewparameter/
+    @Composable
+    private fun CharacterTitle(characterTitle: String) {
+        Surface(color = MaterialTheme.colorScheme.primary) {
+            Text(
+                text = characterTitle,
+                /**
+                 * https://developer.android.com/jetpack/compose/modifiers-list?hl=pt-br
+                 */
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    @Preview(showBackground = true)
+    @Composable
+    private fun Preview() {
+        InitiativeMTheme {
+            CharacterTitle(
+                characterTitle = "Test"
+            )
+        }
+    }
+
+
+//    private fun observeCharacterLiveData() {
+//        detailViewModel.characterLiveData.observe(this) {
+
+//            if (it.isNotEmpty()) {
+//                characterBinding.txtName.text = it[0].name
+//                characterBinding.txtDescription.text = it[0].description
+//                Glide
+//                    .with(this)
+//                    .load(it[0].thumbnail.path + ".jpg")
+//                    .into(characterBinding.thumbnail)
+//            } else {
+//                val buildDialog = AlertDialog.Builder(this)
+//                    .setMessage("Tivemos um erro!")
+//                    .setNegativeButton("Close") { dialog, _ ->
+//                        dialog.dismiss()
+//                    }
+//                buildDialog.create().show()
+//            }
+//        }
+//    }
 }
