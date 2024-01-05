5d4e3d6bc36319202775b96c6f22b6f399b46e2f
Pedro Henrique Rodrigues de Oliveira <prodrigu@lifescan.com>
2024-01-05 17:21:40 -0300

    [INIM-010] Adjust to Jetpack

--- app/src/main/java/com/phro/initiativem/MainActivity.kt
+++ app/src/main/java/com/phro/initiativem/MainActivity.kt
@@ -1,40 +1,79 @@
 package com.phro.initiativem
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.view.View
-import com.phro.initiativem.databinding.ActivityMainBinding
-import com.phro.initiativem.ui.character.DetailCharacterActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.phro.initiativem.ui.ui.theme.InitiativeMTheme
 
+/** TO-DO:
+ * - Remove XML to add the Compose
+ * - Add a Search box to type the character name
+ */
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mainBinding: ActivityMainBinding
-    private lateinit var view: View
-
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        view = mainBinding.root
-
-        setContentView(view)
-
-        onClickedOnAnotherActivity()
-    }
-
-    private fun onClickedOnAnotherActivity() {
-        mainBinding.txtAnotherActivity.setOnClickListener {
-            changeToDetailActivity()
+        setContent {
+            InitiativeMTheme {
+                Surface {
+                    Search()
+                }
+            }
         }
     }
 
-    private fun changeToDetailActivity() {
-        startActivity(
-            Intent(
-                this,
-                DetailCharacterActivity::class.java
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun Search(modifier: Modifier = Modifier) {
+        var text by remember { mutableStateOf("") }
+        var active by remember { mutableStateOf(false) }
+
+        Column(
+            modifier = modifier
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            TextField(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .heightIn(min = 56.dp),
+                value = "",
+                onValueChange = {
+                    text = it
+                },
+                trailingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = null
+                    )
+                },
+                placeholder = {
+                    Text("Character name...")
+                }
             )
-        )
+        }
     }
 }
